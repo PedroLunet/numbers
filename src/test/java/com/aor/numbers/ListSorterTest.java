9a0334df4f2d4ec94311f9ab1e25114d43ed8634
Pedro Lunet <pedrolunet@gmail.com>
2023-10-16 17:56:32 +0100

    Distinct

--- src/test/java/com/aor/numbers/ListSorterTest.java
+++ src/test/java/com/aor/numbers/ListSorterTest.java
@@ -21,4 +21,10 @@ public class ListSorterTest {
         List<Integer> sorted = sorter.sort(list);
         Assertions.assertEquals(expected, sorted);
     }
+    @Test
+    public void bug_sort_8276() {
+        ListSorter sorter = new ListSorter();
+        List<Integer> sorted = sorter.sort(Arrays.asList(1, 2, 4, 2));
+        Assertions.assertEquals(Arrays.asList(1, 2, 2, 4), sorted);
+    }
 }
