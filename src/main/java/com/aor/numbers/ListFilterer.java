96e3e55a9c38b4608b59a84de57089c4a7d47b3a
Pedro Lunet <pedrolunet@gmail.com>
2023-10-23 17:49:43 +0100

    Created Filters

---
+++ src/main/java/com/aor/numbers/ListFilterer.java
@@ -0,0 +1,22 @@
+package com.aor.numbers;
+import java.util.ArrayList;
+import java.util.List;
+
+class ListFilterer{
+
+    private GenericListFilter filter;
+
+    public ListFilterer(GenericListFilter filter) {
+        this.filter = filter;
+    }
+
+    public List<Integer> filter(List<Integer> list) {
+        List<Integer> filteredList = new ArrayList<>();
+        for (Integer number : list) {
+            if (filter.accept(number)) {
+                filteredList.add(number);
+            }
+        }
+        return filteredList;
+    }
+}
